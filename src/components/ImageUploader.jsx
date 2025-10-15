ef4770e79b6b06486d351ba90884b54bea74c01a
Luyue28 <zhan0067@hz.nl>
2025-10-15 15:35:13 +0200

    r2 & r5 - template selection & basic template management

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -58,7 +58,7 @@ export default function ImageUploader() {
                 </label>
 
                 {/* Thumbnails */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-h-[400px] overflow-y-auto">
                     {images.map((img, idx) => (
                         <div
                             key={idx}
@@ -81,6 +81,7 @@ export default function ImageUploader() {
                         </div>
                     ))}
                 </div>
+
 
             </div>
         </div>
